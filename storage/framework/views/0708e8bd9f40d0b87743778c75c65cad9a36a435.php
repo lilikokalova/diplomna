6e91c75254be8cbbd14d8c8955814b07612a0ea2
lilikokalova <lilikokalova@gmail.com>
2016-11-22 19:01:13 +0000

    Add files via upload

---
+++ storage/framework/views/0708e8bd9f40d0b87743778c75c65cad9a36a435.php
@@ -0,0 +1,148 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="<?php echo e(url('/register')); ?>">
+                        <?php echo e(csrf_field()); ?>
+
+
+                        <div class="form-group<?php echo e($errors->has('name') ? ' has-error' : ''); ?>">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="<?php echo e(old('name')); ?>" required autofocus>
+
+                                <?php if($errors->has('name')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('name')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group<?php echo e($errors->has('email') ? ' has-error' : ''); ?>">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required>
+
+                                <?php if($errors->has('email')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('email')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                        <div class="form-group<?php echo e($errors->has('gender') ? ' has-error' : ''); ?>">
+                            <label for="gender" class="col-md-4 control-label">Gender</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="gender" value="<?php echo e(old('gender')); ?>" required autofocus>
+
+                                <?php if($errors->has('gender')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('gender')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                        
+                        <div class="form-group<?php echo e($errors->has('city') ? ' has-error' : ''); ?>">
+                            <label for="city" class="col-md-4 control-label">City</label>
+
+                            <div class="col-md-6">
+                                <input id="city" type="text" class="form-control" name="city" value="<?php echo e(old('city')); ?>" required autofocus>
+
+                                <?php if($errors->has('city')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('city')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                        
+                        
+                        
+                        <div class="form-group<?php echo e($errors->has('country') ? ' has-error' : ''); ?>">
+                            <label for="country" class="col-md-4 control-label">Country</label>
+
+                            <div class="col-md-6">
+                                <input id="country" type="text" class="form-control" name="country" value="<?php echo e(old('country')); ?>" required autofocus>
+
+                                <?php if($errors->has('country')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('country')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        
+                        
+                        
+                        <div class="form-group<?php echo e($errors->has('date_of_birth') ? ' has-error' : ''); ?>">
+                            <label for="date_of_birth" class="col-md-4 control-label">Date of birth</label>
+
+                            <div class="col-md-6">
+                                <input id="date_of_birth" type="text" class="form-control" name="date_of_birth" value="<?php echo e(old('date_of_birth')); ?>" required autofocus>
+
+                                <?php if($errors->has('date_of_birth')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('date_of_birth')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        
+                        
+                        
+
+                        <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                <?php if($errors->has('password')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('password')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
